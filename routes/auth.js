aff2de8a93c4690ee4cc523674b6c485f1058c40
federicoCassettai <f.cassettai@gmail.com>
2019-02-12 13:46:37 +0100

    sort by date

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const mapbox = require("../public/javascripts/geocode");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcryptjs");
@@ -26,6 +27,8 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  const position = req.body.position;
+
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -40,20 +43,34 @@ router.post("/signup", (req, res, next) => {
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
+    mapbox(
+      "pk.eyJ1IjoiZnJkMjZ4IiwiYSI6ImNqcnQ4ZGFzMjF4dDA0M3BzOWg4NGNlem4ifQ.SgF_HKYViz0-nlirZ9Ksag",
+      `${position}`,
+      function(err, data) {
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+        const newUser = new User({
+          username,
+          password: hashPass,
+          position,
+          loc: {
+            type: "Point",
+            coordinates: data.features[0].center
+          }
+ });
+ newUser.save()
+ .then(() => {
+   res.redirect("/");
+ })
+ .catch(err => {
+   res.render("auth/signup", { message: "Something went wrong" });
+ })
+        
+    
+
+  
   });
 });
+})
 
 router.get("/logout", (req, res) => {
   req.logout();
